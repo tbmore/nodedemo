99bc4c33695e5e2997e5b11415ffb8bf40099418
ganjx <ganjx@blizzmi.com>
2021-04-15 12:15:06 +0800

    add

--- config/index.js
+++ config/index.js
@@ -1,4 +1,5 @@
 module.exports = {
-    mongoDBURL: 'mongodb://localhost:27017', // 数据库地址
+    // mongoDBURL: 'mongodb://localhost:27017', // 数据库地址
+    mongoDBURL: 'mongodb+srv://jason:1989360@cluster0.sdzbn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', // 数据库地址
     secretOrPrivateKey:'testdmoe', // jwt 加密密钥
 }
